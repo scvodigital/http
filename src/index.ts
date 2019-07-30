6f14ff58b8dbd611fb331893e953f770ae8390a2
Calum MacUisdean <calum.macuisdean@gmail.com>
2019-07-30 07:51:12 +0100

    header fix

--- src/index.ts
+++ src/index.ts
@@ -452,6 +452,7 @@ const routerRequestHandler = async (request: Http.IncomingMessage, response: Htt
       response.setHeader('Access-Control-Request-Method', '*');
       response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
       response.setHeader('Access-Control-Allow-Headers', '*');
+      response.setHeader('Content-Type', 'text/html; charset=utf-8');
     }
 
     // Send our HTTP response with the GZipped buffer
